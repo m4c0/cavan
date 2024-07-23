1a7d3dafeda49181f30ef61458f7dc8c36176fe6
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-23 18:47:09 +0100

    trying to infer file versions

--- compile.cpp
+++ compile.cpp
@@ -7,31 +7,82 @@
 #include "../leco/sim.hpp"
 
 import cavan;
+import hai;
 import jute;
+import mtime;
+import pprent;
+import silog;
 import yoyo;
 
 using namespace jute::literals;
+
+static void build_path(sim_sb *path, const char *grp, const char *art) {
+  sim_sb_copy(path, getenv("HOME"));
+  sim_sb_path_append(path, ".m2/repository");
+  sim_sb_path_append(path, grp);
+  sim_sb_path_append(path, art);
+}
+
+static auto build_path(sim_sb *path, const char *grp, const char *art,
+                       const char *ver) {
+  build_path(path, grp, art);
+  sim_sb_path_append(path, ver);
+  sim_sb_path_append(path, art);
+  sim_sb_printf(path, "-%s.jar", ver);
+  return mtime::of(path->buffer);
+}
+static void check_path(sim_sb *path, const char *grp, const char *art,
+                       const char *ver) {
+  if (build_path(path, grp, art, ver))
+    return;
+
+  silog::log(silog::error, "file not found: %s", path->buffer);
+  throw 1;
+}
+
+static void find_dep_path(sim_sb *path, const cavan::dep &d) {
+  sim_sbt grp_path{};
+  sim_sb_copy(&grp_path, d.grp.begin());
+  while (auto *c = strchr(grp_path.buffer, '.')) {
+    *c = '/';
+  }
+
+  if (d.ver.begin()) {
+    check_path(path, grp_path.buffer, d.art.begin(), d.ver.begin());
+    return;
+  }
+
+  if (build_path(path, grp_path.buffer, d.art.begin(), "1.0-SNAPSHOT"))
+    return;
+
+  mtime::t max{};
+
+  build_path(path, grp_path.buffer, d.art.begin());
+  for (auto ver : pprent::list(path->buffer)) {
+    sim_sbt tmp{};
+    auto mtime = build_path(&tmp, grp_path.buffer, d.art.begin(), ver);
+
+    if (mtime < max)
+      continue;
+
+    sim_sb_copy(path, tmp.buffer);
+    max = mtime;
+  }
+
+  if (max == 0) {
+    silog::log(silog::error, "dependency not found: %s:%s", d.grp.begin(),
+               d.art.begin());
+    throw 1;
+  }
+}
 
 static void run_java(cavan::deps &deps) {
   for (auto &d : deps) {
     if (d.scp != "compile"_s)
       continue;
 
-    sim_sbt grp_path{};
-    sim_sb_copy(&grp_path, d.grp.begin());
-    while (auto *c = strchr(grp_path.buffer, '.')) {
-      *c = '/';
-    }
-
-    const char *ver = d.ver.begin() ? d.ver.begin() : "1.0-SNAPSHOT";
-
     sim_sbt path{};
-    sim_sb_copy(&path, getenv("HOME"));
-    sim_sb_path_append(&path, grp_path.buffer);
-    sim_sb_path_append(&path, d.art.begin());
-    sim_sb_path_append(&path, ver);
-    sim_sb_path_append(&path, d.art.begin());
-    sim_sb_printf(&path, "-%s.jar", ver);
+    find_dep_path(&path, d);
 
     puts(path.buffer);
   }
