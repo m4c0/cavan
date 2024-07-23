00469e9137abf8c6540d24f98227eaae186e5f27
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-23 12:04:17 +0100

    reading end-tag and texts

--- list-deps.cpp
+++ list-deps.cpp
@@ -26,12 +26,8 @@ struct token {
 };
 using tokens = hai::varray<token>;
 
-static auto take(const char *&buffer, char c) {
-  if (*buffer != c)
-    return mno::req<void>::failed("mismatched char");
-
-  buffer++;
-  return mno::req{};
+static auto blank(const char *c) {
+  return *c == ' ' || *c == '\t' || *c == '\n' || *c == '\r';
 }
 
 static auto read_tag(const char *&b) {
@@ -44,22 +40,71 @@ static auto read_tag(const char *&b) {
     return mno::req<token>::failed("expecting '>' got EOF");
 
   const char *end;
-  for (end = start; end <= b; end++)
-    if (*end == ' ')
+  for (end = start; end < b; end++)
+    if (blank(end))
       break;
 
+  b++; // consume '>'
+
+  auto id = jute::view{start + 1, static_cast<unsigned>(end - start - 1)};
+  return mno::req{token{id.cstr(), T_OPEN_TAG}};
+}
+
+static auto read_end_tag(const char *&b) {
+  return read_tag(b).peek([](auto &t) {
+    t.id = jute::view{t.id}.subview(1).after.cstr();
+    t.type = T_CLOSE_TAG;
+  });
+}
+
+static auto read_text(const char *&b) {
+  const char *start = b;
+  while (*b && *b != '<') {
+    b++;
+  }
+
+  const char *end = b;
+  while (blank(end) && end > start)
+    end--;
+
+  if (end == start)
+    return mno::req<token>{};
+
   auto id = jute::view{start, static_cast<unsigned>(end - start)};
-  return mno::req{token{id.cstr()}};
+  return mno::req{token{id.cstr(), T_TEXT}};
 }
 
 static auto split_tokens(const hai::cstr &cstr) {
   const char *buffer = cstr.begin();
 
-  tokens ts{1024};
-  return take(buffer, '<')
-      .fmap([&] { return read_tag(buffer); })
-      .map([&](token &t) { ts.push_back_doubling(traits::move(t)); })
-      .map([&] { return traits::move(ts); });
+  mno::req<tokens> ts{tokens{1024}};
+  while (*buffer && ts.is_valid()) {
+    mno::req<token> t{};
+
+    switch (*buffer) {
+    case '<':
+      t = (buffer[1] == '/') ? read_end_tag(buffer) : read_tag(buffer);
+      break;
+    case ' ':
+    case '\t':
+    case '\r':
+    case '\n':
+      buffer++;
+      continue;
+    default:
+      t = read_text(buffer);
+      break;
+    }
+
+    ts = mno::combine(
+        [](auto &ts, auto &t) {
+          if (t.type != T_NULL)
+            ts.push_back_doubling(traits::move(t));
+          return traits::move(ts);
+        },
+        ts, t);
+  }
+  return ts;
 }
 
 int main(int argc, char **argv) try {
@@ -89,7 +134,7 @@ int main(int argc, char **argv) try {
       .map([](auto &tokens) {
         silog::log(silog::info, "got %d tokens", tokens.size());
         for (auto &t : tokens) {
-          silog::log(silog::info, "-- [%s]", t.id.begin());
+          silog::log(silog::info, "-- %d [%s]", t.type, t.id.begin());
         }
       })
       .log_error([] { throw 1; });
