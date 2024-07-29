700ff21acaabba9ded5194b6af2e00a67ef93fa5
Eduardo Costa <m4c0@users.noreply.github.com>
2024-07-29 19:43:11 +0100

    pom: remove some indent

--- cavan_pom.cpp
+++ cavan_pom.cpp
@@ -1,6 +1,21 @@
 module cavan;
 import silog;
 
+static mno::req<void> parse_project(const cavan::token *&t, cavan::pom &res) {
+  using namespace cavan;
+
+  if (match(*t, T_OPEN_TAG, "dependencyManagement")) {
+    return take_if(t, "dependencyManagement", [&] {
+      return list_deps(t).map(
+          [&](auto &deps) { res.deps_mgmt = traits::move(deps); });
+    });
+  }
+  if (match(*t, T_OPEN_TAG, "dependencies")) {
+    return list_deps(t).map([&](auto &deps) { res.deps = traits::move(deps); });
+  }
+  return lint_tag(t);
+}
+
 mno::req<cavan::pom> cavan::parse_pom(const cavan::tokens &ts) {
   auto *t = ts.begin();
 
@@ -8,20 +23,7 @@ mno::req<cavan::pom> cavan::parse_pom(const cavan::tokens &ts) {
     return mno::req<pom>::failed("missing <?xml?> directive");
 
   cavan::pom res{};
-  return take(t, "project",
-              [&] -> mno::req<void> {
-                if (match(*t, T_OPEN_TAG, "dependencyManagement")) {
-                  return take_if(t, "dependencyManagement", [&] {
-                    return list_deps(t).map([&](auto &deps) {
-                      res.deps_mgmt = traits::move(deps);
-                    });
-                  });
-                }
-                if (match(*t, T_OPEN_TAG, "dependencies")) {
-                  return list_deps(t).map(
-                      [&](auto &deps) { res.deps = traits::move(deps); });
-                }
-                return lint_tag(t);
-              })
-      .map([&] { return traits::move(res); });
+  return take(t, "project", [&] { return parse_project(t, res); }).map([&] {
+    return traits::move(res);
+  });
 }
