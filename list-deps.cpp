de27ea5eb265e977bb24b0aee17425bf1d5c8b25
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-27 16:30:27 +0100

    list-deps: simplifying

--- list-deps.cpp
+++ list-deps.cpp
@@ -1,49 +1,34 @@
 #pragma leco tool
 
 import cavan;
-import gopt;
 import hai;
+import jojo;
 import jute;
-import traits;
-import yoyo;
 import silog;
 
 using namespace cavan;
 using namespace jute::literals;
 
-static void usage() {
-  // TODO: document usage
-  silog::log(silog::error, "invalid usage");
-  throw 1;
+static void list_deps(void *, hai::cstr & xml) {
+  split_tokens(xml)
+      .fpeek(cavan::lint_xml)
+      .fmap(cavan::parse_pom)
+      .map([](auto & pom) {
+        auto & deps = pom.deps;
+
+        silog::log(silog::info, "found %d dependencies", deps.size());
+
+        for (auto & d : deps) {
+          silog::log(silog::debug, "%s:%s:%s:%s", d.grp.begin(), d.art.begin(), d.ver.begin(), d.scp.begin());
+        }
+      })
+      .log_error([] { throw 1; });
 }
 
-int main(int argc, char **argv) try {
-  auto opts = gopt_parse(argc, argv, "i:", [&](auto ch, auto val) {
-    switch (ch) {
-    case 'i':
-      yoyo::file_reader::open(val)
-          .fmap(cavan::read_tokens)
-          .fpeek(cavan::lint_xml)
-          .fmap(cavan::parse_pom)
-          .map([](auto &pom) {
-            auto &deps = pom.deps;
-
-            silog::log(silog::info, "found %d dependencies", deps.size());
-
-            for (auto &d : deps) {
-              silog::log(silog::debug, "%s:%s:%s:%s", d.grp.begin(),
-                         d.art.begin(), d.ver.begin(), d.scp.begin());
-            }
-          })
-          .log_error([] { throw 1; });
-      break;
-    default:
-      usage();
-    }
-  });
-
-  if (opts.argc != 0)
-    usage();
+int main(int argc, char ** argv) try {
+  for (auto i = 1; i < argc; i++) {
+    jojo::read(jute::view::unsafe(argv[i]), nullptr, list_deps);
+  }
 } catch (...) {
   return 1;
 }
