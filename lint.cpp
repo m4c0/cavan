ac32caba98851a97c6b1f1e43842cc6ddfa60efa
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-27 16:24:27 +0100

    lint: simpler example

--- lint.cpp
+++ lint.cpp
@@ -1,33 +1,19 @@
 #pragma leco tool
 import cavan;
-import gopt;
 import hai;
 import jojo;
 import jute;
-import silog;
 
 using namespace cavan;
-
-static void usage() {
-  // TODO: document usage
-  silog::log(silog::error, "invalid usage");
-  throw 1;
-}
 
 static void lint(void *, hai::cstr & xml) {
   split_tokens(xml).fmap(cavan::lint_xml).log_error([] { throw 1; });
 }
 
 int main(int argc, char ** argv) try {
-  auto opts = gopt_parse(argc, argv, "i:", [&](auto ch, auto val) {
-    switch (ch) {
-      case 'i': jojo::read(jute::view::unsafe(val), nullptr, lint); break;
-      default: usage();
-    }
-  });
-
-  if (opts.argc != 0) usage();
-
+  for (auto i = 1; i < argc; i++) {
+    jojo::read(jute::view::unsafe(argv[i]), nullptr, lint);
+  }
 } catch (...) {
   return 1;
 }
