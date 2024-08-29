ab37a069d441657c58e17440eef6a57fa820339d
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-29 19:52:17 +0100

    statics in cavan_deps without mno

--- cavan_deps.cpp
+++ cavan_deps.cpp
@@ -1,5 +1,6 @@
 module cavan;
 import :fail;
+import silog;
 
 using namespace cavan;
 
@@ -13,10 +14,9 @@ void cavan::take_tag(jute::view exp_id, const token *& t, hai::cstr * out) {
   if (!match(*t, T_CLOSE_TAG, exp_id)) fail("missing close tag for " + exp_id);
 }
 
-[[nodiscard]] static mno::req<void> take_exclusions(const token *&t, hashley::rowan &exc) {
-  hai::cstr grp{};
-  hai::cstr art{};
-  mno::req<void> res{};
+static void take_exclusions(const token *& t, hashley::rowan & exc) try {
+  hai::cstr grp {};
+  hai::cstr art {};
   for (; !match(*t, T_END); t++) {
     if (match(*t, T_OPEN_TAG, "groupId")) {
       take_tag("groupId", t, &grp);
@@ -26,19 +26,19 @@ void cavan::take_tag(jute::view exp_id, const token *& t, hai::cstr * out) {
       auto key = ""_hs + grp + ":" + art + "\0";
       exc[(*key).begin()] = 1;
     } else if (match(*t, T_CLOSE_TAG, "exclusions")) {
-      return {};
+      return;
     }
-    if (!res.is_valid())
-      return res.trace("parsing exclusions");
   }
-  return res;
+} catch (...) {
+  silog::log(silog::info, "while parsing exclusions");
+  throw;
 }
 
-[[nodiscard]] static mno::req<dep> take_dep(const token *&t) {
-  dep d{};
+static dep take_dep(const token *& t) try {
+  dep d {};
 
   for (; t->type != T_END; t++) {
-    mno::req<void> res{};
+    mno::req<void> res {};
     if (match(*t, T_OPEN_TAG, "groupId")) {
       take_tag("groupId", t, &d.grp);
     } else if (match(*t, T_OPEN_TAG, "artifactId")) {
@@ -48,7 +48,7 @@ void cavan::take_tag(jute::view exp_id, const token *& t, hai::cstr * out) {
     } else if (match(*t, T_OPEN_TAG, "scope")) {
       take_tag("scope", t, &d.scp);
     } else if (match(*t, T_OPEN_TAG, "optional")) {
-      hai::cstr tmp{100};
+      hai::cstr tmp { 100 };
       take_tag("optional", t, &tmp);
       res = res.map([&] { d.opt = "true"_s == tmp; });
     } else if (match(*t, T_OPEN_TAG, "classifier")) {
@@ -56,18 +56,17 @@ void cavan::take_tag(jute::view exp_id, const token *& t, hai::cstr * out) {
     } else if (match(*t, T_OPEN_TAG, "type")) {
       take_tag("type", t, &d.typ);
     } else if (match(*t, T_OPEN_TAG, "exclusions")) {
-      res = take_exclusions(t, d.exc);
+      take_exclusions(t, d.exc);
     } else if (match(*t, T_CLOSE_TAG, "dependency")) {
       break;
-    } else {
-      res = mno::req<void>::failed("unknown stuff found inside dependencies");
-    }
-    if (!res.is_valid())
-      return res.map([] { return dep{}; }).trace("parsing dependency");
+    } else fail("unknown stuff found inside dependencies");
   }
 
   // silog::log(silog::debug, "dependency: %s:%s:%s:%s", grp, art, ver, scp);
-  return mno::req{traits::move(d)};
+  return d;
+} catch (...) {
+  silog::log(silog::info, "while parsing dependency");
+  throw;
 }
 
 mno::req<deps> cavan::list_deps(const token *&t) {
@@ -77,8 +76,8 @@ mno::req<deps> cavan::list_deps(const token *&t) {
                    if (!match(*t, T_OPEN_TAG, "dependency"))
                      return mno::req{};
 
-                   return take_dep(++t).map(
-                       [&](auto &d) { res.push_back_doubling(d); });
+                   res.push_back_doubling(take_dep(++t));
+                   return mno::req {};
                  })
       .map([&] { return traits::move(res); });
   ;
