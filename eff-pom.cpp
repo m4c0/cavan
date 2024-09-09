fdf4550b0c14fb1779dba098cd8b6099cdb4d253
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-09 17:37:45 +0100

    eff: resolving deps and dep mgmt

--- eff-pom.cpp
+++ eff-pom.cpp
@@ -13,29 +13,44 @@ struct dd {
   hai::cstr ver {};
   unsigned depth {};
 };
-static hai::varray<dd> g_deps_bucket { 1024000 };
-static hashley::rowan g_deps {};
+class dep_map {
+  hai::varray<dd> m_bucket { 1024000 };
+  hashley::rowan m_deps {};
 
-static void parse_parent(cavan::pom * n, unsigned depth) {
-  for (auto & d : n->deps) {
-    auto id = (jute::view { d.grp } + ":" + d.art).cstr();
-    auto & v = g_deps[id.begin()];
-    if (v == 0) {
-      g_deps_bucket.push_back(dd {
-          .grp = jute::view { d.grp }.cstr(),
-          .art = jute::view { d.art }.cstr(),
-          .ver = jute::view { d.ver }.cstr(),
-          .depth = depth,
-      });
-      v = g_deps_bucket.size();
-      continue;
+public:
+  void take(jute::view grp, jute::view art, jute::view ver, unsigned depth) {
+    auto id = (grp + ":" + art).cstr();
+    auto & idx = m_deps[id.begin()];
+    if (idx == 0) {
+      m_bucket.push_back(dd { grp.cstr(), art.cstr(), ver.cstr(), depth });
+      idx = m_bucket.size();
+      return;
     }
-    auto & dd = g_deps_bucket[v - 1];
-    if (dd.depth <= depth) continue;
 
-    dd.ver = jute::view { d.ver }.cstr();
+    auto & dd = m_bucket[idx - 1];
+    if (dd.depth <= depth) return;
+
+    dd.ver = ver.cstr();
     dd.depth = depth;
   }
+
+  [[nodiscard]] jute::view version_of(jute::view grp, jute::view art) const {
+    auto id = (grp + ":" + art).cstr();
+    auto idx = m_deps[id.begin()];
+    if (idx == 0) return "TBD";
+    return m_bucket[idx - 1].ver;
+  }
+
+  [[nodiscard]] auto begin() const { return m_bucket.begin(); }
+  [[nodiscard]] auto end() const { return m_bucket.end(); }
+};
+
+static dep_map g_dep_map {};
+static dep_map g_dep_mgmt_map {};
+
+static void parse_parent(cavan::pom * n, unsigned depth) {
+  for (auto & d : n->deps) g_dep_map.take(d.grp, d.art, d.ver, depth);
+  for (auto & d : n->deps_mgmt) g_dep_mgmt_map.take(d.grp, d.art, d.ver, depth);
 
   auto & [grp, art, ver] = n->parent;
   if (!grp.size() && !art.size() && !ver.size()) return;
@@ -50,8 +65,9 @@ static void run(void *, hai::cstr & xml) {
   auto pom = cavan::read_pom(xml);
   parse_parent(&pom, 1);
 
-  for (auto & d : g_deps_bucket) {
-    silog::log(silog::info, " - %s:%s:%s", d.grp.begin(), d.art.begin(), d.ver.begin());
+  for (auto & d : g_dep_map) {
+    jute::view ver = (d.ver.size() == 0) ? g_dep_mgmt_map.version_of(d.grp, d.art) : d.ver;
+    silog::log(silog::info, "dependency %s:%s:%s", d.grp.begin(), d.art.begin(), ver.cstr().begin());
   }
 }
 
