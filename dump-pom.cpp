7df1af15f9984e852a76334a5e8613263bb902fb
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-17 11:24:35 +0100

    dump-pom: fix after version with jute::view

--- dump-pom.cpp
+++ dump-pom.cpp
@@ -30,13 +30,13 @@ static void dump_pom(void *, hai::cstr & xml) {
     silog::log(silog::info, "found %d managed dependencies", pom.deps_mgmt.size());
     for (auto & d : pom.deps_mgmt) {
       silog::log(silog::info, "- %10s %s:%s:%s", d.scp.cstr().begin(), d.grp.cstr().begin(), d.art.cstr().begin(),
-                 d.ver.begin());
+                 d.ver.cstr().begin());
     }
 
     silog::log(silog::info, "found %d dependencies", pom.deps.size());
     for (auto & d : pom.deps) {
       silog::log(silog::info, "- %10s %s:%s:%s", d.scp.cstr().begin(), d.grp.cstr().begin(), d.art.cstr().begin(),
-                 d.ver.begin());
+                 d.ver.cstr().begin());
     }
 
     if (pom.parent.grp.size() == 0) break;
