f4e0ee4c527ccae8087ee3a1ab1825a8c07d234c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-20 12:17:33 +0100

    effpom: reusing prop logic

--- cavan_effpom.cpp
+++ cavan_effpom.cpp
@@ -33,29 +33,6 @@ namespace {
     [[nodiscard]] constexpr auto end() const { return m_values.end(); }
     [[nodiscard]] constexpr auto size() const { return m_values.size(); }
   };
-  class propmap : public kvmap<cavan::prop> {
-  public:
-    jute::heap apply(jute::heap str) const {
-      for (unsigned i = 0; i < str.size() - 3; i++) {
-        if ((*str)[i] != '$') continue;
-        if ((*str)[i + 1] != '{') continue;
-
-        unsigned j {};
-        for (j = i; j < str.size() && (*str)[j] != '}'; j++) {
-        }
-
-        if (j == str.size()) return str;
-
-        jute::view before { str.begin(), i };
-        jute::view prop { str.begin() + i + 2, j - i - 2 };
-        jute::view after { str.begin() + j + 1, str.size() - j - 1 };
-
-        str = before + (*this)[prop]->val + after;
-        i--;
-      }
-      return str;
-    };
-  };
   class depmap : kvmap<cavan::dep> {
   public:
     using kvmap::begin;
@@ -75,16 +52,14 @@ namespace {
   class context {
     depmap m_dep_map {};
     depmap m_dep_mgmt_map {};
-    propmap m_props {};
 
     void parse_parent(cavan::pom * n, unsigned depth) {
-      for (auto & d : n->props) m_props.take(d.key, &d, depth);
       for (auto & d : n->deps) m_dep_map.take(&d, depth);
 
       for (auto & d : n->deps_mgmt) {
         if (d.scp == "import"_s) {
           if (!d.pom) {
-            d.ver = m_props.apply(d.ver);
+            d.ver = cavan::apply_props(n, d.ver);
             // silog::log(silog::debug, "importing %s:%s:%s", d.grp.begin(), d.art.begin(), ver.begin());
             d.pom.reset(new cavan::pom { cavan::read_pom(d.grp, d.art, *d.ver) });
           }
@@ -104,7 +79,7 @@ namespace {
 
       for (auto & d : m_dep_map) {
         d->ver = (d->ver.size() == 0) ? m_dep_mgmt_map.dep_of(d->grp, d->art)->ver : d->ver;
-        d->ver = m_props.apply(d->ver);
+        d->ver = cavan::apply_props(pom, d->ver);
 
         silog::log(silog::info, "dependency %s:%s:%s", d->grp.cstr().begin(), d->art.cstr().begin(),
                    (*d->ver).cstr().begin());
