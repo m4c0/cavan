32e73e8e7027effa1511659187809ed0ac9bf34b
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-24 18:05:16 +0100

    compile: using cavan's eff-pom + refactor

--- compile.cpp
+++ compile.cpp
@@ -1,8 +1,6 @@
 #pragma leco tool
-#include <unistd.h>
-
-#define SIM_IMPLEMENTATION
-#include "../leco/sim.hpp"
+#include <stdlib.h>
+#include <string.h>
 
 import cavan;
 import jojo;
@@ -10,20 +8,6 @@ import jute;
 import mtime;
 
 using namespace jute::literals;
-
-static void build_path(sim_sb * path, const char * grp, const char * art) {
-  sim_sb_copy(path, getenv("HOME"));
-  sim_sb_path_append(path, ".m2/repository");
-  sim_sb_path_append(path, grp);
-  sim_sb_path_append(path, art);
-}
-
-static void build_path(sim_sb * path, const char * grp, const char * art, const char * ver, const char * type) {
-  build_path(path, grp, art);
-  sim_sb_path_append(path, ver);
-  sim_sb_path_append(path, art);
-  sim_sb_printf(path, "-%s.%s", ver, type);
-}
 
 static auto infer_base_folder(jute::view src) {
   while (src != "") {
@@ -34,30 +18,22 @@ static auto infer_base_folder(jute::view src) {
   cavan::fail("file not in maven repo");
 }
 
-static void create_efpom(jute::view base) {
-  // TODO: generate effective-pom using cavan
-
-  chdir(base.cstr().begin());
-
-  const auto cmd = "mvn -q -nsu -o -B help:effective-pom -Dverbose=true -Doutput=target/cavan-compile.pom";
-  if (0 != system(cmd)) cavan::fail("maven failed");
-}
-
 static int compile(char * fname) {
   auto src = jute::view::unsafe(fname);
   auto base = infer_base_folder(src);
   auto tgt = base + "/target/classes";
-  auto efpom = (base + "/target/cavan-compile.pom").cstr();
+  auto pom_file = (base + "/pom.xml").cstr();
   auto tmpnam = (base + "/target/cavan-compile.args").cstr();
 
-  if (mtime::of(efpom.begin()) == 0) create_efpom(base);
-
-  auto pom = cavan::read_pom(jojo::read_cstr(efpom));
+  auto pom = cavan::read_pom(jojo::read_cstr(pom_file));
+  cavan::eff_pom(&pom);
 
   jojo::write(tmpnam, "-d "_hs + tgt + "\n");
   jojo::append(tmpnam, "-cp "_hs + tgt);
 
   bool test_scope = strstr(fname, "src/test/");
+
+  jute::heap m2repo { jute::view::unsafe(getenv("HOME")) + "/.m2/repository" };
 
   for (auto & d : pom.deps) {
     if (d.cls != "jar"_s && d.cls != ""_s) continue;
@@ -66,15 +42,16 @@ static int compile(char * fname) {
 
     // TODO: traverse deps
 
-    sim_sbt grp {};
-    sim_sb_copy(&grp, d.grp.begin());
-    while (auto p = strchr(grp.buffer, '.')) *p = '/';
+    auto jar = m2repo;
+    auto grp = d.grp;
+    while (grp.size() > 0) {
+      auto [l, r] = grp.split('.');
+      jar = jar + "/" + l;
+      grp = r;
+    }
+    jar = jar + "/" + d.art + "/" + d.ver + "/" + d.art + "-" + d.ver + "." + d.typ;
 
-    sim_sbt path {};
-    build_path(&path, grp.buffer, d.art.begin(), d.ver.begin(), d.typ.begin());
-
-    jojo::append(tmpnam, ":"_hs);
-    jojo::append(tmpnam, jute::view { path.buffer, path.len });
+    jojo::append(tmpnam, ":"_hs + jar);
   }
 
   jojo::append(tmpnam, "\n"_hs);
