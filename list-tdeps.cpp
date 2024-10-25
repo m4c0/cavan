18cca8821f30258168f6d3f75ee12a01c5d68131
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-25 12:32:42 +0100

    some DPS logic + breadcrumbs for debugging

--- list-tdeps.cpp
+++ list-tdeps.cpp
@@ -2,6 +2,7 @@
 
 import cavan;
 import hai;
+import hashley;
 import jojo;
 import jute;
 import missingno;
@@ -10,31 +11,65 @@ import silog;
 using namespace cavan;
 using namespace jute::literals;
 
-static void add_tdep(cavan::deps & out, const cavan::dep & d, bool test_scope) {
-  auto dpom = cavan::read_pom(*d.grp, d.art, *d.ver);
-  cavan::eff_pom(dpom);
-  for (auto & [d, _] : dpom->deps) {
+struct node {
+  cavan::dep dep;
+  cavan::dep parent;
+  cavan::pom * owner;
+};
+class output {
+  hai::chain<node> m_nodes { 1024 };
+  hashley::niamh m_idx { 1103 };
+
+public:
+  void push_back(node n) {
+    auto key = n.dep.grp + ":" + n.dep.art;
+    auto & i = m_idx[*key];
+    if (i) return;
+    m_nodes.push_back(n);
+    i = m_nodes.size();
+  }
+
+  [[nodiscard]] auto operator[](cavan::dep d) const {
+    auto key = d.grp + ":" + d.art;
+    auto i = m_idx[*key];
+    return i ? m_nodes.seek(i) : node {};
+  }
+
+  auto begin() { return m_nodes.begin(); }
+  auto end() { return m_nodes.end(); }
+};
+
+static void add_deps(output & out, cavan::pom * pom, cavan::dep parent, bool test_scope) {
+  cavan::eff_pom(pom);
+
+  for (auto & [d, _] : pom->deps) {
     if (d.opt) continue;
     if (d.cls != "jar"_s && d.cls != ""_s) continue;
     if (test_scope && d.scp != "test"_s && d.scp != "compile"_s && d.scp != "provided") continue;
     if (!test_scope && d.scp != "compile"_s && d.scp != "provided") continue;
 
-    out.push_back(d);
+    out.push_back({ d, parent, pom });
   }
 }
 
 static void run(jute::view fname) {
+  output out {};
+
   auto pom = cavan::read_pom(fname);
-  cavan::eff_pom(pom);
+  add_deps(out, pom, {}, true);
 
-  cavan::deps deps {};
-  for (auto & [dep, _] : pom->deps) {
-    add_tdep(deps, dep, true);
-  }
-
-  for (auto & [dep, _] : deps) {
-    add_tdep(deps, dep, false);
-  }
+  for (auto & [d, _, owner] : out) try {
+      auto dpom = cavan::read_pom(*d.grp, d.art, *d.ver);
+      add_deps(out, dpom, d, false);
+    } catch (...) {
+      while (owner) {
+        silog::log(silog::error, "while processing deps of %s", owner->filename.begin());
+        auto [_, p, o] = out[d];
+        d = p;
+        owner = o;
+      }
+      throw;
+    }
 }
 
 int main(int argc, char ** argv) try {
